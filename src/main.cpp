9ddcc940423c4e9e38640e4e2eb8f0de9837c81d
hal468a <asdfg880226@gmail.com>
2024-09-28 01:49:13 +0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string.h>
+
+int main(void) {
+	int temp = 0;
+	int f, s = 0;
+
+	// std::cout << "Input an integer: " << std::endl;
+	// std::cout << "> ";
+	// std::cin >> temp;
+
+	temp = __cplusplus % 100000 % 10000;
+	// std::cout << "temp % 10000 % 10000:" << temp << std::endl;
+
+	f = temp / 1000;
+	// std::cout << "temp / 1000 as f:" << f << std::endl;
+
+	s = temp % 1000 / 100;
+	// std::cout << "temp % 1000 / 100 as s:" << s << std::endl;
+
+	std::cout << "This app is using C++" << f << s << std::endl;
+	return 0;
+}
