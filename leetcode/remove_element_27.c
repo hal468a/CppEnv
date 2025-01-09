b7cd22d57976bf9877dbce0fb5ca326af8df6e32
Louis <asdfg880226@gmail.com>
2025-01-10 01:06:04 +0800

    update add Q27

--- leetcode/remove_element_27.c
+++ leetcode/remove_element_27.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 void show(int* nums, int numSize){
     for(int i = 0; i < numSize; i++){
         printf("%d ", nums[i]);
@@ -9,14 +6,9 @@ void show(int* nums, int numSize){
 }
 
 int removeElement(int* nums, int numsSize, int val){
-    int k = 0;
-    for(int i = 0; i < numsSize; i++){
-        if(nums[i] != val){
-            nums[k] = nums[i];
-            k++;
-        }
-    }
-    return k;
+    int cout = 0;
+
+    return cout;
 }
 
 int main(void){
@@ -25,16 +17,13 @@ int main(void){
     int num1[] = {3,2,2,3}; // {2,2}
     int numSize = sizeof(num1) / sizeof(num1[0]);
     int target = 3;
-
-    int k = removeElement(num1, numSize, target);
-    show(num1, numSize - 1);
+    printf("%d ", removeElement(num1, numSize, target));
 
     // Case 2:
     int num2[] = {0,1,2,2,3,0,4,2}; // {0,1,4,0,3}
     numSize = sizeof(num2) / sizeof(num2[0]);
     target = 2;
-    k = removeElement(num2, numSize, target);
-    show(num2, numSize);
+    printf("%d ", removeElement(num2, numSize, target));
 
     return 0;
 }
