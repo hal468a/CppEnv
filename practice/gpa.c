841f0fae71bf4a1a3e1c03757c22aabbd35eeaf3
hal468a <asdfg880226@gmail.com>
2025-01-10 20:06:28 +0800

    update

--- practice/gpa.c
+++ practice/gpa.c
@@ -4,14 +4,13 @@
 int main(void){
 
     int grade[] = {97, 96, 84, 81, 85, 91, 90, 90, 85};
-    double gpa = 0.0;
+    int gpa = 0;
     int numSize = sizeof(grade) / sizeof(grade[0]);
 
     for(int i = 0; i <= numSize; i++){
-        grade[i] = grade[i] * 3;
-        gpa = gpa + grade[i];
+        grade[i] *= 3;
+        gpa += grade[i];
     }
-    
     gpa = gpa / (numSize * 3);
     printf("%.2f", gpa);
 
